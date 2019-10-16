d97e92366921d31dc2603d3d607a8ff423dd98e9
Collin Padgett <collinpadgett@Collins-MBP.domain>
2019-10-15 22:31:17 -0500

    Added activities for tutorial, video, game, forum, and about pages and made an intent for all of them onclick

--- app/src/main/java/com/example/cinc/GameActivity.java
+++ app/src/main/java/com/example/cinc/GameActivity.java
@@ -1,6 +1,7 @@
 package com.example.cinc;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 
 public class GameActivity extends AppCompatActivity {
--- app/src/main/java/com/example/cinc/MainActivity.java
+++ app/src/main/java/com/example/cinc/MainActivity.java
@@ -1,7 +1,8 @@
 package com.example.cinc;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.view.View;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,4 +12,40 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onTutorialPress(View view) {
+
+        Intent intent = new Intent(this, TutorialActivity.class);
+        startActivity(intent);
+
+    }
+
+    public void onVideoPress(View view) {
+
+        Intent intent = new Intent(this, VideoActivity.class);
+        startActivity(intent);
+
+    }
+
+    public void onGamePress(View view) {
+
+        Intent intent = new Intent(this, GameActivity.class);
+        startActivity(intent);
+
+    }
+
+    public void onForumPress(View view) {
+
+        Intent intent = new Intent(this, ForumActivity.class);
+        startActivity(intent);
+
+    }
+
+    public void onAboutPress(View view) {
+
+        Intent intent = new Intent(this, AboutActivity.class);
+        startActivity(intent);
+
+    }
+
 }
