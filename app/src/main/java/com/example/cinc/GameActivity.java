af826f9343da457672deb334f292a05f3d441b21
Collin Padgett <collinpadgett@Collins-MBP.domain>
2019-10-23 17:47:24 -0500

    modified the games page

--- app/src/main/java/com/example/cinc/GameActivity.java
+++ app/src/main/java/com/example/cinc/GameActivity.java
@@ -1,8 +1,9 @@
 package com.example.cinc;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class GameActivity extends AppCompatActivity {
 
@@ -11,4 +12,12 @@ public class GameActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
     }
+
+    public void onPrevPress(View view) {
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+
+    }
+
 }
