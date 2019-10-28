ec543016e36f8e7646e7172f11350eda689d2094
Collin Padgett <collinpadgett@Collins-MBP.domain>
2019-10-28 00:04:11 -0500

    added to forum page

--- app/src/main/java/com/example/cinc/ForumActivity.java
+++ app/src/main/java/com/example/cinc/ForumActivity.java
@@ -1,14 +1,32 @@
 package com.example.cinc;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class ForumActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forum);
+
+        WebView webview = (WebView)findViewById(R.id.webview);
+        webview.setWebViewClient(new WebViewClient());
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.loadUrl("http://www.google.com");
+
     }
+
+    public void onPrevPress(View view) {
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+
+    }
+
 }
